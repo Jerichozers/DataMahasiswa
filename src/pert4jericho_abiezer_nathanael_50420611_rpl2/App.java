3b1a68bac6847e5dc8672d84a451762b3a899f08
Muhammad Fata Nuryanto <jerichoabiezers@gmail.com>
2023-12-02 16:17:29 +0700

    Upload Project

---
+++ src/pert4jericho_abiezer_nathanael_50420611_rpl2/App.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pert4jericho_abiezer_nathanael_50420611_rpl2;
+
+/**
+ *
+ * @author Jericho Abiezer N
+ */
+public class App {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new BarangView().setVisible(true);
+    }
+    
+}
